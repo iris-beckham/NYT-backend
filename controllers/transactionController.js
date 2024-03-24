96ac18146d9eb4110f7fe31726ef2297d359949a
Iris Beckham <irisdancer3@gmail.com>
2024-03-24 15:59:48 -0700

    delete transaction route

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const transactions = express.Router()
-const { getAllTransactions, getSingleTransaction } = require('../queries/transaction.js')
+const { getAllTransactions, getSingleTransaction, deleteTransaction } = require('../queries/transaction.js')
 
 // INDEX
 transactions.get('/', async (req, res) => {
@@ -20,6 +20,17 @@ transactions.get('/:id', async (req, res) => {
         res.status(200).json(singleTransaction);
     } else {
         res.status(500).json({ error: 'server error' });
+    }
+})
+
+//DELETE
+transactions.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedTransaction = await deleteTransaction(id);
+    if (deletedTransaction.id) {
+        res.status(200).json(deletedTransaction);
+    } else {
+        res.status(404).json('Color not found');
     }
 })
 
