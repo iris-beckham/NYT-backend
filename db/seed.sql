ad2ae99522e8fbae5113884aa5d91f08925b81fd
Jennifer <jenniferpeterson@pursuit.org>
2024-06-08 11:45:24 -0400

    seeded the presidents data

--- db/seed.sql
+++ db/seed.sql
@@ -1,20 +1,13 @@
-\c budget_dev; 
+\c teamtwo
 
-
--- Create your seedfiles here using SQL -  INSERT INTO() VALUES()
-
-INSERT INTO transactions ( amount, date, transaction_from, category, memo, pending, account, recurring) 
-VALUES 
-( 3000.00, '2024-03-20', 'Employer', 'Income', 'good job', false, 'USAA Savings', true),
-(-1300.00,'2024-03-17', 'Landlord', 'Rent', '', false, 'Chase', false),
-(-50.00, '2024-03-19', 'Restaurant', 'Entertainment', 'Lobster dinner', true, 'Amex', false),
-(1500.00, '2024-03-25', 'Freelance Client', 'Income', 'Website development project', false, 'PayPal', false),
-(-75.00, '2024-03-22', 'Trader Joes', 'Groceries', '', false, 'USAA Checking Account', false),
-(-200.00, '2024-03-21', 'Electric Company', 'Utilities', 'Electricity bill', true, 'Amex', false),
-(-25.50, '2024-03-26', 'Sweet Treat Bakery', 'Groceries', 'Birthday Cake', false, 'Chase', false),
-(-150.00, '2024-03-28', 'Water Company', 'Utilities', '', false, 'Amex', false),
-(-65.00, '2024-03-30', 'Internet Service Provider', 'Utilities', '', false, 'Amex', false),
-(-50.00, '2024-03-31', 'Gas Company', 'Utilities', 'Natural gas bill', true, 'Chase', false)
-;
-
-
+INSERT INTO presidents (pres_name, term_start, term_end, terms, party, image) VALUES
+('Richard Nixon', '1969-01-20', '1974-08-09', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717787937/220px-Richard_M._Nixon_2C_ca._1935_-_1982_-_NARA_-_530679_au8dga.jpg'),
+('Gerald Ford', '1974-08-09', '1977-01-20', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717787969/220px-Gerald_Ford_28cropped_29_j5zy4r.jpg'),
+('Jimmy Carter', '1977-01-20', '1981-01-20', 1, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717787992/220px-JimmyCarterPortrait2_rpk5pm.jpg'),
+('Ronald Reagan', '1981-01-20', '1989-01-20', 2, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788120/220px-Official_Portrait_of_President_Reagan_1981_jxzz9a.jpg'),
+('George H. W. Bush', '1989-01-20', '1993-01-20', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788163/220px-George_H._W._Bush_[…]_States_2C_1989_official_portrait_cropped_28b_29_g8gafs.jpg'),
+('Bill Clinton', '1993-01-20', '2001-01-20', 2, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788290/220px-Bill_Clinton_ucizgb.jpg'),
+('George W. Bush', '2001-01-20', '2009-01-20', 2, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788355/220px-George-W-Bush_hcyioi.jpg'),
+('Barack Obama', '2009-01-20', '2017-01-20', 2, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788389/220px-President_Barack_Obama_rhidwc.jpg'),
+('Donald Trump', '2017-01-20', '2021-01-20', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788403/220px-Donald_Trump_official_portrait_joaogo.jpg'),
+('Joe Biden', '2021-01-20', '2025-01-20', 1, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788451/Joe_Biden_presidential_portrait_kqktpy.jpg');
