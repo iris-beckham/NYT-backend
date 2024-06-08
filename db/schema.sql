38ed3fef8bb44c2c444693b0db3584464d97d787
Jennifer <jenniferpeterson@pursuit.org>
2024-06-08 11:43:14 -0400

    added two tables to the schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,17 +1,13 @@
-DROP DATABASE IF EXISTS budget_dev; 
-CREATE DATABASE budget_dev; 
+DROP DATABASE IF EXISTS teamtwo; -- change name of db
+CREATE DATABASE teamtwo; -- change name of db
 
-\c budget_dev;
+\c teamtwo
 
-CREATE TABLE transactions (
-    id SERIAL PRIMARY KEY, 
-    amount DECIMAL(7,2), 
-    date DATE, 
-    transaction_from TEXT, 
-    category TEXT,
-    memo TEXT,
-    pending BOOLEAN,
-    account VARCHAR(40),
-    recurring BOOLEAN,
-    transaction_id UUID DEFAULT gen_random_uuid()
-    );
+CREATE TABLE presidents (id SERIAL PRIMARY KEY, pres_name VARCHAR(100) NOT NULL, term_start DATE NOT NULL, term_end DATE NOT NULL, terms NUMERIC NOT NULL, party VARCHAR(100), image VARCHAR(255));
+
+CREATE TABLE PopularBabyNames (
+    Year INT NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    Name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (Year, Gender)
+);
