dc3a84f3adecba99704f25398f9df3e297fe2101
KenCab123 <KennethCabral@pursuit.org>
2024-06-08 21:30:13 -0400

    fixed some images

--- db/seed.sql
+++ db/seed.sql
@@ -5,12 +5,12 @@ INSERT INTO presidents (pres_name, term_start, term_end, terms, party, image) VA
 ('Gerald Ford', '1974-08-09', '1977-01-20', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717787969/220px-Gerald_Ford_28cropped_29_j5zy4r.jpg'),
 ('Jimmy Carter', '1977-01-20', '1981-01-20', 1, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717787992/220px-JimmyCarterPortrait2_rpk5pm.jpg'),
 ('Ronald Reagan', '1981-01-20', '1989-01-20', 2, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788120/220px-Official_Portrait_of_President_Reagan_1981_jxzz9a.jpg'),
-('George H. W. Bush', '1989-01-20', '1993-01-20', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788163/220px-George_H._W._Bush_[…]_States_2C_1989_official_portrait_cropped_28b_29_g8gafs.jpg'),
+('George H. W. Bush', '1989-01-20', '1993-01-20', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/t_pres/v1717788163/220px-George_H._W._Bush_2C_President_of_the_United_States_2C_1989_official_portrait_cropped_28b_29_g8gafs.jpg'),
 ('Bill Clinton', '1993-01-20', '2001-01-20', 2, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788290/220px-Bill_Clinton_ucizgb.jpg'),
 ('George W. Bush', '2001-01-20', '2009-01-20', 2, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788355/220px-George-W-Bush_hcyioi.jpg'),
 ('Barack Obama', '2009-01-20', '2017-01-20', 2, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788389/220px-President_Barack_Obama_rhidwc.jpg'),
 ('Donald Trump', '2017-01-20', '2021-01-20', 1, 'Republican', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788403/220px-Donald_Trump_official_portrait_joaogo.jpg'),
-('Joe Biden', '2021-01-20', '2025-01-20', 1, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/v1717788451/Joe_Biden_presidential_portrait_kqktpy.jpg');
+('Joe Biden', '2021-01-20', '2025-01-20', 1, 'Democrat', 'https://res.cloudinary.com/djg5i10dg/image/upload/t_pres/v1717789851/640px-Joe_Biden_presidential_portrait_v6jzns.jpg');
 
 INSERT INTO PopularBabyNames (Year, Gender, Name) VALUES 
 (1974, 'Boy', 'Michael'),
@@ -110,4 +110,6 @@ INSERT INTO PopularBabyNames (Year, Gender, Name) VALUES
 (2021, 'Boy', 'Liam'),
 (2021, 'Girl', 'Olivia'),
 (2022, 'Boy', 'Liam'),
-(2022, 'Girl', 'Olivia');
+(2022, 'Girl', 'Olivia'),
+(2023, 'Boy', 'Liam'),
+(2023, 'Girl', 'Olivia');
